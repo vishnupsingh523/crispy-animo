ba6910ac40332b8dc2924a4d1749984472965d15
Vishwanath Pratap Singh <vishnupsingh523@gmail.com>
2021-11-14 18:51:29 +0530

    files modified

---
+++ first.js
@@ -0,0 +1,10 @@
+var name = 'Vishwanath'
+var age = 20
+var hasHobbies = true;
+
+function summarizeUser(userName, userAge, userHasHObby){
+    return ('My name is '+userName+', age is '+userAge+
+    ' and the user has hobbies: '+userHasHObby);
+}
+
+console.log(summarizeUser(name,age,hasHobbies))
